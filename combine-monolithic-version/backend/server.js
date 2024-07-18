3614d002412c8d43a054adac432aef678376c0f2
berkarslann <berkk.arsln@gmail.com>
2024-07-18 09:31:40 +0300

    feat: add CORS configuration and MongoDB connection
    - Added CORS configuration to allow cross-origin requests from the frontend.
    - Implemented MongoDB connection using environment variables.
    - Configured Vercel for deployment with a vercel.json file.

--- combine-monolithic-version/backend/server.js
+++ combine-monolithic-version/backend/server.js
@@ -1,25 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
-import feedRoutes from './routes/feed.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import feedRoutes from "./routes/feed.js";
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://combine-api.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
-app.use('/auth', authRoutes);
-app.use('/feed', feedRoutes);
+app.use("/auth", authRoutes);
+app.use("/feed", feedRoutes);
 
 const PORT = 3000;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("MongoDB bağlantısı başarılı");
     app.listen(PORT, () => {
       console.log(`Server ${PORT} numaralı portta çalışıyor`);
