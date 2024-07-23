36d8b712c16cb4090b06b9ea13b4aded9c294be4
berkarslann <berkk.arsln@gmail.com>
2024-07-23 12:10:18 +0300

    fix: resolve deployment issues

--- combine-monolithic-version/backend/server.js
+++ combine-monolithic-version/backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://combine-navy.vercel.app"],
+    origin: ["http://localhost:3000", "https://combine-navy.vercel.app", "https://combine-unlg.onrender.com/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
--- combine-monolithic-version/frontend/src/pages/login/login.jsx
+++ combine-monolithic-version/frontend/src/pages/login/login.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { MainContainer, LoginContainer, LoginInput, LoginButton } from "./styles";
+import {
+  MainContainer,
+  LoginContainer,
+  LoginInput,
+  LoginButton,
+} from "./styles";
 import logo from "../../assets/logo.png";
 import LoginNavbar from "../../layouts/navbar/login/navbar";
 import { loginUser, getCurrentUser } from "../../redux/user/user.action";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -21,9 +27,9 @@ const Login = () => {
       const response = await dispatch(loginUser(userCre));
       localStorage.setItem("accessToken", response.payload.accessToken);
       dispatch(getCurrentUser());
-      navigate('/feed');
+      navigate("/feed");
     } catch (err) {
-      setError('Hatalı kullanıcı adı veya şifre');
+      setError("Hatalı kullanıcı adı veya şifre");
       setInputError(true);
       setTimeout(() => setInputError(false), 2000); // 2 saniye sonra hatayı sıfırla
       console.log(err);
@@ -32,7 +38,7 @@ const Login = () => {
 
   useEffect(() => {
     if (currentUser) {
-      navigate('/feed');
+      navigate("/feed");
     }
   }, [currentUser, navigate]);
 
@@ -45,15 +51,15 @@ const Login = () => {
           type="email"
           placeholder="E-posta"
           onChange={(e) => setEmail(e.target.value)}
-          style={{ borderColor: inputError ? 'red' : '' }}
+          style={{ borderColor: inputError ? "red" : "" }}
         />
         <LoginInput
           type="password"
           placeholder="Şifre"
           onChange={(e) => setPassword(e.target.value)}
-          style={{ borderColor: inputError ? 'red' : '' }}
+          style={{ borderColor: inputError ? "red" : "" }}
         />
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <h3>
           Giriş yaparak{" "}
           <a
@@ -67,12 +73,12 @@ const Login = () => {
         <LoginButton onClick={sendAuthInfo}>giriş yap</LoginButton>
         <h5>
           Hesabınız yok mu? Bir projeye eşleşmek için hemen{" "}
-          <a
-            href="/signup"
+          <Link
+            to="/signup"
             style={{ textDecoration: "underline", color: "purple" }}
           >
             kaydolun
-          </a>
+          </Link>
           .
         </h5>
       </LoginContainer>
